3e3439d2c815397bcb2d550b60e4c0aab0f795b1
SHIVANSHU TYAGI <74995570+ShivanshuGT@users.noreply.github.com>
2026-05-07 16:01:04 +0530

    Add: trapping-rain-water - submission-6

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-6.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] nextGreatest = new int[n];
+        int[] prevGreatest = new int[n];
+
+        int greatest = height[n-1];
+        nextGreatest[n-1] = -1;
+        for(int i = n-2; i >= 0; i--){
+            if(height[i] > greatest){
+                nextGreatest[i] = -1;
+                greatest = height[i];
+            }else{
+                nextGreatest[i] = greatest;
+            }
+        }
+        for(int x : nextGreatest){
+            System.out.print(x + " ");
+        }
+
+        greatest = height[0];
+        prevGreatest[0] = -1;
+        for(int i = 1; i < n; i++){
+            if(height[i] > greatest){
+                prevGreatest[i] = -1;
+                greatest = height[i];
+            }else{
+                prevGreatest[i] = greatest;
+            }
+        }
+        System.out.println();
+        for(int x : prevGreatest){
+            System.out.print(x + " ");
+        }
+
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            int val = Math.min(prevGreatest[i], nextGreatest[i]);
+            if(val > 0){
+                ans += (val - height[i]);
+            }
+        }
+        return ans;
+    }
+}
