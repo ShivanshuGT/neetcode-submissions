bfb750d61098023069fe53869690bd0c880624f9
SHIVANSHU TYAGI <74995570+ShivanshuGT@users.noreply.github.com>
2026-05-09 14:17:15 +0530

    Add: car-fleet - submission-17

---
+++ Data Structures & Algorithms/car-fleet/submission-17.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = position.length;
+        for(int i = 0; i < n; i++){
+            map.put(position[i], speed[i]);
+        }
+
+        Arrays.sort(position);
+        int ans = 1;
+        double time = 0;
+        time = (double)(target - position[n-1])/(map.get(position[n-1]));
+        for(int i = n-2; i >= 0; i--){
+            int sp = map.get(position[i]);
+            double pos = (double)(position[i] + (time * sp));
+            if(pos >= target){
+                continue;
+            }else{
+                ans += 1;
+                time = (double)(target - position[i])/(map.get(position[i]));
+            }
+                
+        }
+        return ans;
+    }
+}
