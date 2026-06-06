4b43838f796eb64202e974b407cabaa76f57924b
SHIVANSHU TYAGI <74995570+ShivanshuGT@users.noreply.github.com>
2026-06-06 13:04:52 +0530

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    private boolean isPalindrome(String s){
+        int n = s.length();
+        int left = 0;
+        int right = n-1;
+        while(left <= right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left +=1 ;
+            right -= 1;
+        }
+        return true;
+    }
+
+    private void helper(String s, List<String> ls, List<List<String>> ans){
+        int n = s.length();
+        if(s.isEmpty()){
+            ans.add(new ArrayList<>(ls));
+            return;
+        }
+        for(int i = 1; i <= n; i++){
+            boolean left = isPalindrome(s.substring(0, i));
+            if(left){
+                ls.add(s.substring(0, i));
+                helper(s.substring(i), ls, ans);
+                ls.removeLast();
+            }
+        }
+    }
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        List<String> ls = new ArrayList<>();
+        helper(s, ls, ans);
+        return ans;
+        
+    }
+}
