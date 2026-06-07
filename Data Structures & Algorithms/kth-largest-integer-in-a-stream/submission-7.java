8c45dc6042a87db337f788a7591a84f3679a1d0c
SHIVANSHU TYAGI <74995570+ShivanshuGT@users.noreply.github.com>
2026-06-07 13:04:13 +0530

    Add: kth-largest-integer-in-a-stream - submission-7

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-7.java
@@ -0,0 +1,49 @@
+class KthLargest {
+
+    private PriorityQueue<Integer> queue; 
+    private int size;
+    private int capacity;
+
+    public KthLargest(int k, int[] nums) {
+        this.queue = new PriorityQueue<>();
+        int n = nums.length;
+        if(n > 0){
+            nums = Arrays.stream(nums).boxed().sorted((a, b) -> b-a).mapToInt(Integer::intValue).toArray();
+
+            // place top k elements in the queue
+            int x = k;
+            int y = 0;
+            while(x > 0 && y < n){
+                this.queue.add(nums[y]);
+                y += 1;
+                x -= 1;
+            }
+            
+        }
+
+        this.size = Math.min(n, k);
+        this.capacity = k;
+    }
+    
+    public int add(int val) {
+        if(!this.queue.isEmpty()){
+            Integer top = this.queue.peek();
+            if(val >= top){
+                if(this.size == this.capacity){
+                    this.queue.poll();
+                    this.queue.add(val);
+                }else{
+                    this.queue.add(val);
+                    this.size += 1;
+                }
+            }
+            return this.queue.peek();
+        }else{
+            this.queue.add(val);
+            this.size += 1;
+            return this.queue.peek();
+        }
+        
+        
+    }
+}
