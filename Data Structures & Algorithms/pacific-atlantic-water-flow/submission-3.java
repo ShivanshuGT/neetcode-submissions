0db927091dd22b72e504981998f7da7ef4941e50
SHIVANSHU TYAGI <74995570+ShivanshuGT@users.noreply.github.com>
2026-06-13 15:04:17 +0530

    Add: pacific-atlantic-water-flow - submission-3

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-3.java
@@ -0,0 +1,66 @@
+class Solution {
+
+    private boolean validCell(int n, int m , int i, int j){
+        return !(i < 0 || i >= n || j < 0 || j >= m);
+    }
+
+    private int[][] dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+
+    private void helper(int[][] heights, int[][] mat, int i, int j){
+        int n = heights.length;
+        int m = heights[0].length;
+
+        for(int[] dir : dirs){
+            int i1 = i + dir[0];
+            int j1 = j + dir[1];
+
+            if(validCell(n, m, i1, j1) && mat[i1][j1] < 10 && heights[i1][j1] >= heights[i][j]){
+                if(mat[i1][j1] != mat[i][j]){
+                    mat[i1][j1] += mat[i][j];
+                    helper(heights, mat, i1, j1);
+                }
+                
+            }
+        }
+   
+    }
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+
+        int n = heights.length;
+        int m = heights[0].length;
+        int[][] mat = new int[n][m];
+        // score for pacific = 3
+        // score for atlantic = 10
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i == 0 || j == 0){
+                    mat[i][j] += 3;
+                }
+
+                if(i == n-1 || j == m-1){
+                    mat[i][j] += 7;
+                }
+            }
+        }
+
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                helper(heights, mat, i, j);
+            }
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(mat[i][j] >= 10){
+                    ans.add(List.of(i, j));
+                }
+            }
+        }
+        return ans;
+
+        
+    }
+}
