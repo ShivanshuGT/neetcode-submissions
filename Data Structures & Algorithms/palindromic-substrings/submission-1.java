bfeaf6e430c890f1f029d119d67be7ab67ff04f4
SHIVANSHU TYAGI <74995570+ShivanshuGT@users.noreply.github.com>
2026-06-25 19:03:07 +0530

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int ans = 0;
+
+        int n = s.length();
+        // count the first char of 's' as a palindrome
+        ans += 1;
+
+        for(int i = 1; i < n; i++){
+            int low = i;
+            int high = i;
+
+            while(low >= 0 && high < n && (s.charAt(low) == s.charAt(high))){
+                low -= 1;
+                high += 1;
+                ans += 1;
+
+            }
+
+            low = i-1;
+            high = i;
+            while(low >= 0 && high < n && (s.charAt(low) == s.charAt(high))){
+                low -= 1;
+                high += 1;
+                ans += 1;
+
+            }
+
+
+        }
+        return ans;
+        
+    }
+}
