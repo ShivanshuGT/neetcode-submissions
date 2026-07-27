1f3764aa0deea4ec88270de734b0afbd56a7b23d
SHIVANSHU TYAGI <74995570+ShivanshuGT@users.noreply.github.com>
2026-07-27 11:09:56 +0530

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+
+        // counting sort 
+        // TC -> O(n + k)
+        // SC -> O(k)
+        // where 'k' is the number of unique elements
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int n = nums.length;
+
+        for(int i = 0; i < n; i++){
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+
+        }
+
+        int x = 0;
+
+        for(int i = min; i <= max; i++){
+            int freq = map.getOrDefault(i, 0);
+            while(freq > 0){
+                nums[x] = i;
+                x += 1;
+                freq -= 1;
+            }
+        }
+        return nums;
+        
+    }
+}
