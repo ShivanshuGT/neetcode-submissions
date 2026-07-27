95d8b39cf81979edfe861e63f73627418bc385bd
SHIVANSHU TYAGI <74995570+ShivanshuGT@users.noreply.github.com>
2026-07-27 11:21:35 +0530

    Add: sort-an-array - submission-3

---
+++ Data Structures & Algorithms/sort-an-array/submission-3.java
@@ -0,0 +1,52 @@
+class Solution {
+
+    // Merge Sort
+    // TC -> O(n x log(n))
+    // SC -> O(n)
+
+    private void merge(int[] arr, int beg, int mid, int end){
+        List<Integer> temp = new ArrayList<>();
+
+        int left = beg;
+        int right = mid+1;
+
+        while(left <= mid && right <= end){
+            if(arr[left] <= arr[right]){
+                temp.add(arr[left]);
+                left += 1;
+            }else{
+                temp.add(arr[right]);
+                right += 1;
+            }
+        }
+
+        while(left <= mid){
+            temp.add(arr[left]);
+            left += 1;
+        }
+
+        while(right <= end){
+            temp.add(arr[right]);
+            right += 1;
+        }
+
+        for(int i = beg; i <= end; i++){
+            arr[i] = temp.get(i - beg);
+        }
+    }
+
+    private void mergeSort(int[] arr, int beg, int end){
+        if(beg == end){
+            return ;
+        }
+
+        int mid = (beg + end) / 2;
+        mergeSort(arr, beg, mid);
+        mergeSort(arr, mid+1, end);
+        merge(arr, beg, mid, end);
+    }
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length-1);
+        return nums;
+    }
+}
