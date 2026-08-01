284f6b14ad570b1c23874d76add1cf7636b333c3
SHIVANSHU TYAGI <74995570+ShivanshuGT@users.noreply.github.com>
2026-08-01 13:21:20 +0530

    Add: implement-queue-using-stacks - submission-2

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-2.java
@@ -0,0 +1,57 @@
+class MyQueue {
+
+    Stack<Integer> primary; // for pushing the elements
+    Stack<Integer> secondary; // for poping the elements
+
+    public MyQueue() {
+        this.primary = new Stack<>();
+        this.secondary = new Stack<>();
+    }
+    
+    public void push(int x) {
+        this.primary.push(x);
+    }
+    
+    public int pop() {
+        if(this.secondary.isEmpty() && this.primary.isEmpty()){
+            return -1;
+        }
+
+        if(!this.secondary.isEmpty()){
+            return this.secondary.pop();
+        }
+
+        while(!this.primary.isEmpty()){
+            this.secondary.push(this.primary.pop());
+        }
+        return this.secondary.pop();
+    }
+    
+    public int peek() {
+        if(this.secondary.isEmpty() && this.primary.isEmpty()){
+            return -1;
+        }
+
+        if(!this.secondary.isEmpty()){
+            return this.secondary.peek();
+        }
+
+        while(!this.primary.isEmpty()){
+            this.secondary.push(this.primary.pop());
+        }
+        return this.secondary.peek();
+    }
+    
+    public boolean empty() {
+        return this.primary.isEmpty() && this.secondary.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
