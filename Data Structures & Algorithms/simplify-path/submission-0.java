8c78f9b425d38e89b073cb52104ab5cca199036b
SHIVANSHU TYAGI <74995570+ShivanshuGT@users.noreply.github.com>
2026-08-02 12:47:17 +0530

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String[] tokens = path.split("/");
+        for(String token : tokens){
+            System.out.println(token);
+        }
+
+        for(String token : tokens){
+            if("".equals(token) || ".".equals(token)){
+                continue;
+            }else if("..".equals(token)){
+                if(stack.isEmpty()){
+                    continue;
+                }else{
+                    stack.pop();
+                }
+            }else{
+                stack.push(token);
+            }
+        }
+
+        String result = "";
+        while(!stack.isEmpty()){
+            result = "/" + stack.pop() + result;
+        }
+        if("".equals(result)){
+            return "/";
+        }
+        return result;
+    }
+}
