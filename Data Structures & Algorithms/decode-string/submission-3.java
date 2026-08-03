13791c5412daac3b3355711657e0daec41f25137
SHIVANSHU TYAGI <74995570+ShivanshuGT@users.noreply.github.com>
2026-08-03 19:43:52 +0530

    Add: decode-string - submission-3

---
+++ Data Structures & Algorithms/decode-string/submission-3.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<String> stack = new Stack<>();
+
+        int n = s.length();
+        int i = 0;
+
+        while(i < n){
+            if("]".equals(s.substring(i, i+1))){
+                String encodedString = stack.pop();
+                while(!"[".equals(stack.peek())){
+                    encodedString = stack.pop() + encodedString;
+                }
+                stack.pop();
+                String kstr = new String();
+                while(!stack.isEmpty() && Character.isDigit(stack.peek().charAt(0))){
+                    kstr = stack.pop() + kstr; 
+                }
+                int k = Integer.valueOf(kstr);
+                StringBuilder str = new StringBuilder();
+                while(k != 0){
+                    str.append(encodedString);
+                    k -= 1;
+                }
+                stack.push(str.toString());
+            }else{
+                stack.push(s.substring(i, i+1));
+            }
+            i += 1;
+            
+        }
+
+        String ans = "";
+
+        while(!stack.isEmpty()){
+            ans = stack.pop() + ans;
+        }
+        return ans;
+        
+    }
+}
