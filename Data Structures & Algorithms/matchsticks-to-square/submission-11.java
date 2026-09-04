e13a89ad964c43d1bd004b27bb60dcbdcc4fb353
SHIVANSHU TYAGI <74995570+ShivanshuGT@users.noreply.github.com>
2026-09-04 13:10:35 +0530

    Add: matchsticks-to-square - submission-11

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-11.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    private boolean recursive(int[] arr, int ind, int[] sides, int max){
+        int n = arr.length;
+        if(ind >= n){
+            return true;
+        }
+
+        for(int i = 0; i < 4; i++){
+            if((sides[i] + arr[ind]) <= max){
+                sides[i] += arr[ind];
+                if(recursive(arr, ind+1, sides, max)){
+                    return true;
+                }
+                sides[i] -= arr[ind];
+            }
+        }
+        return false;
+    }
+    public boolean makesquare(int[] matchsticks) {
+        int n = matchsticks.length;
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += matchsticks[i];
+        }
+
+        if(! (sum % 4 == 0)){
+            return false;
+        }
+
+        int max = sum / 4;
+
+        for(int i = 0; i < n; i++){
+            if(matchsticks[i] > max){
+                return false;
+            }
+        }
+
+
+        int[] sides = new int[4];
+        return recursive(matchsticks, 0, sides, sum/4);
+        
+    }
+}
