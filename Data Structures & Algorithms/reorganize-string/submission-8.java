c5a9e6d1a2489b38b1ac9c811cbdb98fae49619c
SHIVANSHU TYAGI <74995570+ShivanshuGT@users.noreply.github.com>
2026-09-07 19:54:10 +0530

    Add: reorganize-string - submission-8

---
+++ Data Structures & Algorithms/reorganize-string/submission-8.java
@@ -0,0 +1,62 @@
+class QueueEntry{
+    int freq;
+    char ch;
+
+    QueueEntry(int freq, char ch){
+        this.freq = freq;
+        this.ch = ch;
+    }
+
+
+    private int getFrequency(){
+        return this.freq;
+    }
+
+    private char getCh(){
+        return this.ch;
+    }
+}
+class Solution {
+    public String reorganizeString(String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+        for(char x : s.toCharArray()){
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        Comparator<QueueEntry> comp = Comparator.comparingInt(QueueEntry::getFrequency).reversed();
+                                
+
+        PriorityQueue<QueueEntry> queue = new PriorityQueue<>(comp);
+        for(Map.Entry<Character, Integer> entry: map.entrySet()){
+            queue.add(new QueueEntry(entry.getValue(), entry.getKey()));
+        }
+
+        QueueEntry prev = null;
+
+        while(!queue.isEmpty() || prev != null){
+
+            if(prev != null && queue.isEmpty()){
+                return "";
+            }
+            QueueEntry entry = queue.poll();
+            char ch = entry.getCh();
+            sb.append(ch);
+            int freq = entry.getFrequency();
+
+            if(prev != null){
+                queue.add(prev);
+                prev = null;
+            }
+            
+            if(freq > 1){
+                prev = new QueueEntry(freq-1, ch);
+            }
+        }
+        return sb.toString();
+        
+        
+    }
+}
