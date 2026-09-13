5cbb780ce3268c665bb5a4fa377f3e9e9bd10774
SHIVANSHU TYAGI <74995570+ShivanshuGT@users.noreply.github.com>
2026-09-13 13:59:50 +0530

    Add: course-schedule-iv - submission-6

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-6.java
@@ -0,0 +1,50 @@
+class Solution {
+    private List<List<Integer>> buildGraph(int n, int[][] edges){
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            ans.add(new ArrayList<>());
+        }
+
+        for(int[] edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            ans.get(u).add(v);
+        }
+        return ans;
+    }
+
+    private Set<Integer> dfs(int node, List<List<Integer>> graph, Map<Integer, Set<Integer>> map){
+        map.put(node, new HashSet<>());
+
+        Set<Integer> set = new HashSet<>();
+        List<Integer> neighbors = graph.get(node);
+        for(int neighbor: neighbors){
+            set.add(neighbor);
+            if(!map.containsKey(neighbor)){
+                set.addAll(dfs(neighbor, graph, map));
+            }else{
+                set.addAll(map.get(neighbor));
+            }
+        }
+        map.put(node, set);
+        return set;
+        
+    }
+
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        List<List<Integer>> graph = buildGraph(numCourses, prerequisites);
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+
+        for(int i = 0; i < numCourses; i++){
+            if(!map.containsKey(i)){
+                dfs(i, graph, map);
+            }
+        }
+
+        List<Boolean> ans = new ArrayList<>();
+        for(int[] query : queries){
+            ans.add(map.get(query[0]).contains(query[1]));
+        }
+        return ans;
+    }
+}
